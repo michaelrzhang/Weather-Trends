1ac3705513249978659f3c4eb90b3b8b9a3f343a
Michael Zhang <michaelzhang333@gmail.com>
2015-01-15 20:26:05 -0800

    Implement correct parsing of city name

--- src/UrlContent.java
+++ src/UrlContent.java
@@ -5,6 +5,11 @@ import java.net.URL;
 import java.net.URLConnection;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+// http://juliusdavies.ca/json-simple-1.1.1-javadocs/org/json/simple/JSONObject.html JSON simple documentation
 
 // things to add:
 // Global warming detector
@@ -12,18 +17,24 @@ import org.json.simple.JSONObject;
 public class UrlContent{
     public static void main(String[] args) throws Exception{
 
-        JSONObject obj=new JSONObject();
-        obj.put("num",new Integer(100));
-        System.out.print(obj);
-
        
         URL weather_data = new URL("http://api.openweathermap.org/data/2.5/weather?id=5350734");
         URLConnection yc = weather_data.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(
-                                    yc.getInputStream()));
+                                    yc.getInputStream()));  // Grabs info from site
+        String total_info = "";
         String inputLine;
         while ((inputLine = in.readLine()) != null) 
-            System.out.println(inputLine);
+            total_info += inputLine;
+        System.out.println(total_info);
+        JSONParser parser=new JSONParser();
+        Object obj=parser.parse(total_info);
+        JSONObject obj1 = (JSONObject) obj;
+        System.out.println("debug");
+        System.out.println("getting name");
+        String name = (String)obj1.get("name");
+        System.out.println(name);
+
         in.close();
 
     }
